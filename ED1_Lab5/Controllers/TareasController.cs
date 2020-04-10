2127e832f84b38975b6de5bf5ff27bee577a2593
tjfv02 <tjfv02@gmail.com>
2020-04-10 16:58:12 -0600

    Carga de Datos

---
+++ ED1_Lab5/Controllers/TareasController.cs
@@ -0,0 +1,111 @@
+﻿using ED1_Lab5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ED1_Lab4.Controllers
+{
+    public class TareasController : Controller
+    {
+        public static List<TareaPendiente> CargaTareas = new List<TareaPendiente>();
+        // GET: Tareas
+        public ActionResult Index()
+        {
+            return View(CargaTareas);
+        }
+
+        // GET: Tareas/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //--------------CARGA DE DATOS------------------
+        // GET: Tareas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tareas/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                TareaPendiente NuevoPendiente = new TareaPendiente()
+                {
+                    Titulo = collection["Titulo"],
+                    Proyecto = collection["Proyecto"],
+                    Descripcion = collection["Descripcion"],
+                    Prioridad = Convert.ToInt16(collection["Prioridad"]),
+                    FechaEntrega = collection["FechaEntrega"]
+
+                };
+                CargaTareas.Add(NuevoPendiente);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tareas/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Tareas/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tareas/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(CargaTareas);
+        }
+
+        // POST: Tareas/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                TareaPendiente EliminarPendiente = new TareaPendiente()
+                {
+                    Id = id,
+                    Titulo = collection["Titulo"],
+                    Proyecto = collection["Proyecto"],
+                    Descripcion = collection["Descripcion"],
+                    Prioridad = Convert.ToInt16(collection["Prioridad"]),
+                    FechaEntrega = collection["FechaEntrega"]
+
+                };
+                CargaTareas.Remove(EliminarPendiente);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ED1_Lab5/Models/TareaPendiente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ED1_Lab5.Models
+{
+    public class TareaPendiente
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Proyecto { get; set; }
+        public int Prioridad { get; set; }
+        public string FechaEntrega { get; set; }
+
+    }
+}
